235407a54a07b690af1f2d73dc9082e4ac818a3f
Sabir <12734@holbertonstudents.com>
2026-06-02 23:57:28 +0400

    ES6 classes, task 10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -5,7 +5,12 @@ export default class Car {
         this._color = color
     }
 
+    static get [Symbol.species]() {
+        return this;
+    }
+
     cloneCar() {
-        return new Car()
+        const Species = this.constructor[Symbol.species]
+        return new Species();
     }
 }
