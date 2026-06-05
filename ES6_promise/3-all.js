b7e5290948cebfc1d040313b2924c23827bed892
Sabir <12734@holbertonstudents.com>
2026-06-05 21:00:22 +0400

    ES_6 promises, task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { createUser, uploadPhoto } from "./utils";
+
+export default function handleProfileSignup(createUser, uploadPhoto) {
+    let base = {}
+    let user = {}
+    uploadPhoto
+        .then((data) => base = data)
+        .catch(() => console.log('Signup system offline'))
+
+    createUser
+        .then((data) => user = data)
+        .catch(() => console.log('Signup system offline'))
+
+    console.log(`${base.body} ${user.firstName} ${user.lastName}`)
+}
