f3056b0bbc18731f7275f4ab18f427530f4d5bd2
Sabir <12734@holbertonstudents.com>
2026-06-05 20:37:53 +0400

    ES_6 promises, task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(bool) {
     return new Promise((resolve, reject) => {
         if (bool == true) {
-            resolve({status: 200, body: 'success'})
+            resolve({status: 200, body: 'Success'})
         } else {
             reject(new Error('The fake API is not working currently'))
         }
